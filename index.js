cccac3e1124de3a36c0a27f71abd97d1c3d65327
Jongbo Im <whdqh1550@gmail.com>
2022-11-27 02:03:59 -0600

    added babel to use sexy code and did some dev Dependency

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 //package.json can be created by //this folder can be created by putting npm init
+//import express from "express"; // this only can be done because we have babel if we dont have should be const express = require("express");
 
-console.log("Hello NodeJS");
+console.log("How are you??");
