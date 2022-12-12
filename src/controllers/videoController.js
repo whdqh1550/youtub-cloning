fa6680bffdba939c196040b3d5b6b65619152e53
Jongbo Im <whdqh1550@gmail.com>
2022-12-11 19:05:16 -0600

    pug with include extends and block

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,9 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" }); //this is giving title to top of the page
 export const search = (req, res) => res.send("search Videos");
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => res.render("watch", { pageTitle: "watch" });
 export const remove = (req, res) => {
   console.log(req.params);
   res.send("delete video");
 };
-export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("edit video ");
-};
+export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
 export const upload = (req, res) => res.send("upload");
