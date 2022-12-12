ea4ca7b7ebd7beca20a970e672a155c597863f50
Jongbo Im <whdqh1550@gmail.com>
2022-12-11 22:07:15 -0600

    put fake vid info to mixin and use mixin to return html

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: "Jongbo",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+      rating: 5,
+      comment: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Hello2",
+      rating: 5,
+      comment: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Hello3",
+      rating: 5,
+      comment: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos }); //this is giving title to top of the page
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser }); //this is giving title to top of the page
 export const search = (req, res) => res.send("search Videos");
 export const see = (req, res) => res.render("watch", { pageTitle: "watch" });
 export const remove = (req, res) => {
